4c2f048da19054d5f23bbe7d149fe6ff7768dac9
ailton.msj <ailton.msj@gmail.com>
2022-03-06 12:49:01 -0300

    Refactor Clients

--- src/main/java/br/com/amsj/grpc/greeting/client/GreetingClient.java
+++ src/main/java/br/com/amsj/grpc/greeting/client/GreetingClient.java
@@ -12,10 +12,25 @@ public class GreetingClient {
     public static void main(String[] args) {
 
         System.out.println("Hello I'm a gRPC client");
+        new GreetingClient().run();
+    }
+
+    private void run(){
+
         // config the channel
         ManagedChannel channel = ManagedChannelBuilder.forAddress( "localhost", 50051)
                 .usePlaintext()
                 .build();
+
+        unaryGreet(channel);
+
+        // Shutdown the request client
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+
+    }
+
+    private void unaryGreet(ManagedChannel channel){
 
         System.out.println("Creating Stub");
         // create client service ( synchronous )
@@ -23,21 +38,17 @@ public class GreetingClient {
 
         // create protocol buffer to send the message
         Greeting greeting = Greeting.newBuilder()
-                        .setFirstName("Test")
-                                .setLastName("da Silva")
-                                        .build();
+                .setFirstName("Test")
+                .setLastName("da Silva")
+                .build();
 
         GreetRequest greetRequest = GreetRequest.newBuilder()
-                        .setGreeting(greeting)
-                                .build();
+                .setGreeting(greeting)
+                .build();
 
         // send the message and get the response
         GreetResponse greetResponse = greetClient.greet(greetRequest);
         // print the response
         System.out.println(greetResponse.getResult());
-
-        // Shutdown the request client
-        System.out.println("Shutting down channel");
-        channel.shutdown();
     }
 }
--- src/main/java/br/com/amsj/grpc/greeting/client/GreetingManyTimesClient.java
+++ src/main/java/br/com/amsj/grpc/greeting/client/GreetingManyTimesClient.java
@@ -9,10 +9,25 @@ public class GreetingManyTimesClient {
     public static void main(String[] args) {
 
         System.out.println("Hello I'm a gRPC client");
+        new GreetingManyTimesClient().run();
+
+    }
+
+    private void run(){
         // config the channel
         ManagedChannel channel = ManagedChannelBuilder.forAddress( "localhost", 50051)
                 .usePlaintext()
                 .build();
+
+        clientStreaming(channel);
+
+        // Shutdown the request client
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+
+    }
+
+    private void clientStreaming(ManagedChannel channel) {
 
         System.out.println("Creating Stub");
         // create client service ( synchronous )
@@ -20,22 +35,18 @@ public class GreetingManyTimesClient {
 
         // create protocol buffer to send the message
         Greeting greeting = Greeting.newBuilder()
-                        .setFirstName("Test")
-                                .setLastName("da Silva")
-                                        .build();
+                .setFirstName("Test")
+                .setLastName("da Silva")
+                .build();
 
         GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
-                        .setGreeting(greeting)
-                                .build();
+                .setGreeting(greeting)
+                .build();
 
         // send the message and get the response (stream)
         greetClient.greetManyTimes(greetManyTimesRequest)
                 .forEachRemaining(greetManyTimesResponse -> {
                     System.out.println(greetManyTimesResponse.getResult());
                 });
-
-        // Shutdown the request client
-        System.out.println("Shutting down channel");
-        channel.shutdown();
     }
 }
